a56695f79674b1f6e63f59c26d215b36a5a3d64d
Austin Loveless <austinloveless@admins-MacBook-Pro.local>
2023-10-04 16:27:10 -0600

    chart argument

--- eks/argocd.tf
+++ eks/argocd.tf
@@ -1,6 +1,7 @@
 resource "helm_release" "argocd" {
   name             = "argocd"
   repository       = "https://github.com/argoproj/argo-cd/archive/refs/tags/v2.8.4.tar.gz"
+  chart            = "argocd"
   namespace        = "argocd"
   create_namespace = true
 }
--- eks/crossplane.tf
+++ eks/crossplane.tf
@@ -1,6 +1,7 @@
 resource "helm_release" "crossplane" {
   name             = "crossplane"
   repository       = "https://github.com/crossplane/crossplane/archive/refs/tags/v1.13.2.tar.gz"
+  chart            = "crossplane/crossplane"
   namespace        = "crossplane-system"
   create_namespace = true
 }
