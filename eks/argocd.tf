262b50e07bba3a397641fda68a06eeb2ac9a1b18
Austin Loveless <austinloveless@admins-MacBook-Pro.local>
2023-10-04 16:45:15 -0600

    install

--- eks/argocd.tf
+++ eks/argocd.tf
@@ -1,5 +1,9 @@
-resource "helm_release" "argocd" {
+resource "helm_release" "argocd_helm" {
   name             = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+  version          = "3.35.4"
+  values           = [file("argocd.yaml")]
 }
--- eks/crossplane.tf
+++ eks/crossplane.tf
@@ -1,5 +1,6 @@
-resource "helm_release" "crossplane" {
+resource "helm_release" "crossplane_helm" {
   name             = "crossplane"
   repository       = "https://charts.crossplane.io/stable"
   chart            = "crossplane-stable"
+  version          = "1.13.2"
 }
