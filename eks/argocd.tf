08d9b354f2813e02f07889fb6327c1ebe4fbeea4
Austin Loveless <austinloveless@admins-MacBook-Pro.local>
2023-10-04 17:40:10 -0600

    sf

--- eks/argocd.tf
+++ eks/argocd.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "argocd" {
   name             = "argocd"
-  chart            = "argocd/argo-cd"
+  chart            = "argo-cd"
   namespace        = "argocd"
   create_namespace = true
   version          = "3.35.4"
--- eks/crossplane.tf
+++ eks/crossplane.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "crossplane_helm" {
   name             = "crossplane-system"
-  chart            = "crossplane/crossplane-stable"
+  chart            = "crossplane-stable"
   version          = "1.13.2"
   verify           = false
 }
