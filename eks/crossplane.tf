31269bd01d78ee1327144230ef572e375cacaca4
Austin Loveless <austinloveless@admins-MacBook-Pro.local>
2023-10-04 15:56:40 -0600

    trailing slash

--- eks/crossplane.tf
+++ eks/crossplane.tf
@@ -1,6 +1,6 @@
 resource "helm_release" "crossplane" {
   name             = "crossplane"
-  repository       = "https://charts.crossplane.io/stable"
+  repository       = "https://charts.crossplane.io/stable/"
   chart            = "crossplane-stable/crossplane"
   namespace        = "crossplane-system"
   create_namespace = true
