b3b18e489f27ee1828c3acc437c8a99d7e97ea02
Austin Loveless <austinloveless@admins-MacBook-Pro.local>
2023-10-04 15:45:26 -0600

    name of crossplane

--- eks/crossplane.tf
+++ eks/crossplane.tf
@@ -1,7 +1,7 @@
 resource "helm_release" "crossplane" {
   name             = "crossplane"
   repository       = "https://charts.crossplane.io/stable"
-  chart            = "crossplane"
+  chart            = "crossplane-stable"
   namespace        = "crossplane-system"
   create_namespace = true
 
