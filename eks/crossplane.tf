dbf651030cca4ae45b698de289cd89237d09ad41
Austin Loveless <austinloveless@admins-MacBook-Pro.local>
2023-10-04 16:00:32 -0600

    naming

--- eks/crossplane.tf
+++ eks/crossplane.tf
@@ -1,4 +1,4 @@
-resource "kubectl_manifest" "argocd" {
+resource "kubectl_manifest" "crossplane" {
     yaml_body = file("${path.module}/crossplane_install.yaml")
     depends_on = [data.http.wait_for_cluster]
 }
