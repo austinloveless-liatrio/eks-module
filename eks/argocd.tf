322b9c9390389732d5a6cdc6cbb7f12533def0a8
Austin Loveless <austinloveless@admins-MacBook-Pro.local>
2023-10-04 15:48:31 -0600

    local path

--- eks/argocd.tf
+++ eks/argocd.tf
@@ -1,4 +1,4 @@
 resource "kubectl_manifest" "argocd" {
-    yaml_body = file("https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml")
+    yaml_body = file("${path.module}/argocd_install.yaml")
     depends_on = [data.http.wait_for_cluster]
 }
