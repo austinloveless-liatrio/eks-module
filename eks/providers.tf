824265ed3e3bdfe6d4e7150e4feec55d5fdd5f3e
Austin Loveless <austinloveless@admins-MacBook-Pro.local>
2023-10-05 11:18:18 -0600

    helm auth

--- eks/providers.tf
+++ eks/providers.tf
@@ -28,15 +28,11 @@ terraform {
     }
 }
 provider "helm" {
-  repository_cache = "/tf"
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-      command     = "aws"
-    }
+    token                  = data.aws_eks_cluster_auth.this.token
+    load_config_file       = false
   }
 }
 
