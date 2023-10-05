9d2b49ec5c378ab8ec8102bfde04fb8fa42e3c6b
Austin Loveless <austinloveless@admins-MacBook-Pro.local>
2023-10-04 18:13:29 -0600

    manually

--- eks/crossplane.tf
+++ eks/crossplane.tf
@@ -1,5 +1,20 @@
-resource "helm_release" "crossplane_helm" {
-  name             = "crossplane-system"
-  chart            = "https://github.com/crossplane/crossplane/tree/master/cluster/charts/crossplane"
-  verify           = false
+# resource "helm_release" "crossplane" {
+#   name             = "crossplane-system"
+#   chart            = "https://github.com/crossplane/crossplane/tree/master/cluster/charts/crossplane"
+#   verify           = false
+# }
+
+resource "null_resource" "add_crossplane" {
+  provisioner "local-exec" {
+    command = "helm repo add crossplane-stable https://charts.crossplane.io/stable"
+  }
 }
+
+resource "null_resource" "install_crossplane" {
+  provisioner "local-exec" {
+    command = "helm install crossplane --namespace crossplane-system --create-namespace crossplane-stable/crossplane"
+  }
+  depends_on = [null_resource.add_crossplane]
+
+}
+
