e9abcdb7b78df4b7b772d387235320d0cb53af67
Austin Loveless <austinloveless@admins-MacBook-Pro.local>
2023-10-05 11:20:52 -0600

    token

--- eks/providers.tf
+++ eks/providers.tf
@@ -32,7 +32,6 @@ provider "helm" {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
     token                  = data.aws_eks_cluster_auth.this.token
-    load_config_file       = false
   }
 }
 
