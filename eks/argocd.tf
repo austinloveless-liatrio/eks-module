307bd3943f4a6fc2cd15d8f0aa6f508394ca6cbd
Austin Loveless <austinloveless@admins-MacBook-Pro.local>
2023-10-05 11:24:34 -0600

    crossplane

--- eks/argocd.tf
+++ eks/argocd.tf
@@ -1,11 +1,8 @@
 resource "helm_release" "argocd" {
-  name       = "argocd"
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
-  version    = "5.46.4"
-
-  timeout          = 1200
-  force_update     = true
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  version          = "5.46.4"
   namespace        = "argocd"
   create_namespace = true
 }
--- eks/crossplane.tf
+++ eks/crossplane.tf
@@ -1,11 +1,8 @@
-# resource "helm_release" "crossplane" {
-#   name             = "crossplane"
-#   repository       = "https://charts.crossplane.io/stable"
-#   chart            = "crossplane"
-#   verify           = false
-#   namespace        = "crossplane-system"
-#   create_namespace = true
-
-#   timeout      = 1200
-#   force_update = true
-# }
+resource "helm_release" "crossplane" {
+  name             = "crossplane"
+  repository       = "https://charts.crossplane.io/stable"
+  chart            = "crossplane"
+  verify           = false
+  namespace        = "crossplane-system"
+  create_namespace = true
+}
