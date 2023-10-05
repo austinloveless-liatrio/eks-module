62e056ffb51250c58b2a9a75ad531e0621d1714c
Austin Loveless <austinloveless@admins-MacBook-Pro.local>
2023-10-04 18:18:47 -0600

    install helm

--- eks/crossplane.tf
+++ eks/crossplane.tf
@@ -4,10 +4,22 @@
 #   verify           = false
 # }
 
+resource "null_resource" "install_helm" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+      chmod 700 get_helm.sh
+      ./get_helm.sh
+    EOT
+    interpreter = ["/bin/bash", "-c"]
+  }
+}
+
 resource "null_resource" "add_crossplane" {
   provisioner "local-exec" {
     command = "helm repo add crossplane-stable https://charts.crossplane.io/stable"
   }
+    depends_on = [null_resource.install_helm]
 }
 
 resource "null_resource" "install_crossplane" {
